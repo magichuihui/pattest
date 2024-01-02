d22bf97a2955177c3261c670e34a9f0aeb86ba3d
kyra <magichuihui@gmail.com>
2024-01-02 19:02:07 +0800

    feat: 7-2 partitial answer

---
+++ programs/7-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int str_to_minutes(int time);
+
+int main() {
+    int time, delta;
+    scanf("%d %d", &time, &delta);
+
+    int minutes_from_0 = str_to_minutes(time) + delta;
+    printf("%d%d", minutes_from_0/60, minutes_from_0%60);
+
+}
+
+int str_to_minutes(int time) {
+    return time / 100 * 60 + time % 100;
+}
