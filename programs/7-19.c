99b51eae8b5b57cb4172b4989d54bd823cabce38
magichuihui <magichuihui@gmail.com>
2024-01-05 12:56:07 +0800

    fix: 7-19 add comment

--- programs/7-19.c
+++ programs/7-19.c
@@ -5,6 +5,7 @@ int main() {
     scanf("%d", &n);
 
     int f, y;
+    // 100f + y - n = 200y + 2f => 98f - n = 199y
     for(f=1;f<100;f++) {
         if(98 * f - n >= 0 ) {
             if ((98 * f - n) % 199 ==0) {
