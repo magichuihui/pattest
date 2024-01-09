7fe492e0cbdbe626403e624ffe36226ce48186b2
kyra <magichuihui@gmail.com>
2024-01-09 18:32:39 +0800

    feat: 7-37 solution

--- basic/programs/7-37.c
+++ basic/programs/7-37.c
@@ -7,6 +7,9 @@ typedef struct Stack {
     int top;
 } Stack;
 
+void Decompose(Stack *s, int N, int remain, int min);
+void print_stack(Stack s, int N);
+
 void push(Stack *s, int element) {
     // 栈满
     if (s->top > MAX_SIZE - 1) return;  
@@ -26,6 +29,8 @@ int full(Stack *s) {
     return s->top > MAX_SIZE - 1;
 }
 
+int time=0;
+
 int main() {
     int N;
     scanf("%d", &N);
@@ -34,17 +39,39 @@ int main() {
     s.top = 0;
       
 
-    int i, j;
-    // 第一个数不能大于N/2
-    while (i <= N/2) {
- 
+    Decompose(&s, N, N, 1);
+}
+
+void print_stack(Stack s, int N) {
+    int i;
+    printf("%d=", N);
+    for (i=0;i<s.top;i++) {
+        if (i==0) {
+            printf("%d", s.data[i]);
+        } else {
+            printf("+%d", s.data[i]);
+        }
+    }
+    time++;
+    if (time % 4 == 0 || s.data[0] == N) {
+        printf("\n");
+    } else {
+        printf(";");
     }
 }
 
-void Decompose(int n, int min) {
+void Decompose(Stack *s, int N, int remain, int min) {
     int i;
-    if (min > n / 2) return;
-    for (i=min; i<=n/2; i++) {
-
+    if (remain == 0) {
+        print_stack(*s, N);
+        return;
+    }
+    if (min > remain) {
+        return;
+    }
+    for (i=min; i<=remain; i++) {
+        push(s, i);
+        Decompose(s, N, remain - i, i);
+        pop(s);
     }
 }
