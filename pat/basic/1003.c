c3f7fa4e449b68018cc9a0c87ea04dcb9587d99a
magichuihui <magichuihui@gmail.com>
2024-01-10 00:26:26 +0800

    fix: pat 1003

--- pat/basic/1003.c
+++ pat/basic/1003.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_SIZE 102
+#define MAX_SIZE 101
 
 int pass(char *str);
 
 int main() {
     int N;
-    char answers[MAX_SIZE][10];
+    char answers[10][MAX_SIZE];
 
     scanf("%d", &N);
     int i;
@@ -15,12 +15,13 @@ int main() {
         scanf("%s", answers[i]);
     }
 
-    for(i=0;i<N;i++) printf("%s\n", answers[i], pass(answers[i]) ? "YES" : "NO");
+    for(i=0;i<N;i++) printf("%s\n", pass(answers[i]) ? "YES" : "NO");
 }
 
 int pass(char *str) {
     int length = strlen(str);
     int i;
+    // printf("%s\n", str);
 
     // P T 的位置
     int p = 0, t = 0;
@@ -40,7 +41,9 @@ int pass(char *str) {
     if (t-p < 2) return 0;
 
     // 对于aPbATca，尾部的A个数要不少于头部
-    if (p > length - 1 -t) return 0;
+    //if (t - p > 2) {
+        if (p + p * (t-p-2) != length - 1 -t) return 0;
+    //}
 
     return 1;
 }
