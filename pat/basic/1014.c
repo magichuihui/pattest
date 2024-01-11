3a73413e1aa920640502e133a88e9d21cc3f6443
magichuihui <magichuihui@gmail.com>
2024-01-11 23:55:32 +0800

    fix: pat 1014

--- pat/basic/1014.c
+++ pat/basic/1014.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-char * day_of_week(char cipher[4][61]);
+void day_and_hour(char line1[], char line2[], char *day, char *hour);
+char * day_of_week(char day);
+int hour_of_day(char hour);
+int minute(char line3[], char line4[]);
 
 int main() {
     char cipher[4][61];
@@ -10,15 +13,39 @@ int main() {
         scanf("%s", &cipher[i]);
     }
 
-    printf("%s\n", day_of_week(cipher));
+    char day, hour;
+    day_and_hour(cipher[0], cipher[1], &day, &hour);
+
+    printf("%s %02d:%02d\n", day_of_week(day), hour_of_day(hour), minute(cipher[2], cipher[3]));
 }
 
-char * day_of_week(char cipher[][61]) {
-    const char* day[7] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+void day_and_hour(char line1[], char line2[], char *day, char *hour) {
+    int i = 0;
+    while (line1[i] < 'A' || line1[i] > 'G' || line1[i] != line2[i]) {
+        i++;
+    }
+    *day = line1[i++];
 
-    int i=0;
-    while (cipher[0][i] < 'A' || cipher[0][i] > 'Z' || cipher[0][i] != cipher[1][i]) i++;
+    while (((line1[i] < 'A' || line1[i] > 'N') && (line1[i] < '0' || line1[i] > '9')) || line1[i] != line2[i]) i++;
 
-    return (char *) day[cipher[0][i] - 'A'];
+    *hour = line1[i];
+}
 
+char * day_of_week(char day) {
+    const char* days[7] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+
+    return (char *) days[day - 'A'];
+}
+
+int hour_of_day(char hour) {
+    if (hour >= '0' && hour <= '9') return hour - '0';
+    if (hour >= 'A' && hour <= 'N') return hour - 'A' + 10;
+    return 0;
+}
+
+int minute(char line3[], char line4[]) {
+    int i = 0;
+    while(((line3[i] < 'A' || line3[i] > 'Z') && (line3[i] < 'a' || line3[i] > 'z')) || line3[i] != line4[i]) i++;
+
+    return i;
 }
