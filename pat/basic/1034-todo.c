275d9287d5b364ea27f29350a67be9fbf5c58cea
magichuihui <magichuihui@gmail.com>
2024-01-17 00:15:12 +0800

    todo: 1034 gcd bug
    gcd has bug used in function product..

--- pat/basic/1034-todo.c
+++ pat/basic/1034-todo.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 typedef struct rational {
-    int a, b;
+    long long a, b;
 } rational;
 
 void print_num(rational x);
-int gcd(int a, int b);
+long long gcd(long long a, long long b);
 rational simply(rational x);
 rational sum(rational x, rational y);
 rational difference(rational x, rational y);
@@ -14,7 +14,7 @@ rational quotient(rational x, rational y);
 
 int main(void) {
     rational x={0}, y={0};
-    scanf("%d/%d %d/%d", &x.a, &x.b, &y.a, &y.b);
+    scanf("%lld/%lld %lld/%lld", &x.a, &x.b, &y.a, &y.b);
 
     x = simply(x);
     y = simply(y);
@@ -50,19 +50,25 @@ int main(void) {
 }
 
 rational simply(rational x) {
-    int sign=1, g;
-    if (x.a == 0) return x;
-    if (x.a < 0) sign=-1;
-    g = gcd(sign * x.a, x.b);
+    long long g;
+    if (x.a == 0 || x.b == 0) return x;
+    g = gcd(x.a, x.b);
     x.a = x.a / g;
     x.b = x.b / g;
+
+    // 如果y.a为负，需要反转分子分母的符号
+    if (x.b < 0) {
+        x.a *= -1;
+        x.b *= -1;
+    }
 
     return x;
 }
 
 rational sum(rational x, rational y) {
     rational res = {0};
-    res.a = x.a * y.b + y.a * x.b;
+    //long long g = gcd (x.b, y.b);
+    res.a = y.b * x.a + x.b * y.a;
     res.b = x.b * y.b;
 
     return simply(res);
@@ -70,44 +76,52 @@ rational sum(rational x, rational y) {
 
 rational difference(rational x, rational y) {
     rational res = {0};
-    res.a = x.a * y.b - y.a * x.b;
-    res.b = x.b * y.b;
+    long long g = gcd (x.b, y.b);
+    res.a = y.b / g * x.a - x.b / g * y.a;
+    res.b = x.b / g * y.b;
 
     return simply(res);
 }
 
 rational product(rational x, rational y) {
+    if (x.a == 0) return x;
+    if (y.a == 0) return y;
+
     rational res = {0};
-    int g1 = gcd(x.a, y.b);
-    int g2 = gcd(y.a, x.b);
-    res.a = x.a/g1 * y.a/g2;
-    res.b = x.b/g1 * y.b/g2;
+    // long long g1 = gcd(x.a, y.b);
+    // long long g2 = gcd(y.a, x.b);
+    // res.a = (x.a/g1) * (y.a/g2);
+    // res.b = (x.b/g1) * (y.b/g2);
+    res.a = x.a * y.a;
+    res.b = x.b * y.b;
 
     return simply(res);
 }
 
 rational quotient(rational x, rational y) {
-    int g1 = gcd(x.a, y.a);
-    int g2 = gcd(y.b, x.b);
+    if (y.a == 0) {
+        x.b = 0;
+        return x;
+    }
+    if (x.a == 0) return x;
+
 
     rational res = {0};
-    res.a = x.a/g1 * y.b/g2;
-    res.b = x.b/g2 * y.a/g1;
-    // 如果y.a为负，需要反转分子分母的符号
-    if (res.b < 0) {
-        res.a = -1 * res.a;
-        res.b = -1 * res.b;
-    }
+    res.a = x.a * y.b;
+    res.b = x.b * y.a;
+
     return simply(res);
 }
 
-int gcd(int a, int b) {
-    int temp;
+long long gcd(long long a, long long b) {
+    long long temp;
     while (b) {
         temp = b;
         b = a % b;
         a = temp;
     }
+
+    if (a<0) return -1 * a;
 
     return a;
 }
@@ -117,18 +131,18 @@ void print_num(rational x) {
         printf("Inf");
         return;
     }
-    int sign = x.a < 0 ? -1 : 1;
+    long long sign = x.a < 0 ? -1 : 1;
     x.a = sign * x.a;
     if (sign == -1) printf("(-");
 
     // 分母为1 或者 分子为0
     if (x.b == 1 || x.a == 0) {
-        printf("%d", x.a);
+        printf("%lld", x.a);
     } else {
-        int integral = x.a/x.b, remainder = 0;
-        if (integral) printf("%d ", integral);
+        long long integral = x.a/x.b, remainder = 0;
+        if (integral) printf("%lld ", integral);
         remainder = x.a % x.b;
-        printf("%d/%d", remainder, x.b);
+        printf("%lld/%lld", remainder, x.b);
     }
 
     if (sign == -1) printf(")");
