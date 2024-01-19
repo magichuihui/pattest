f8c15df2a9cd5363145f551af8ce4a1393c25b7d
kyra <magichuihui@gmail.com>
2024-01-19 18:38:40 +0800

    todo: pat 1045 partial error

--- pat/basic/1045.c
+++ pat/basic/1045.c
@@ -12,29 +12,26 @@ int main(void) {
     int number[MAX_SIZE], primary[MAX_SIZE];
     scanf ("%d", &N);
     
-    int i, j, count=0;
-
-    for (i=0;i<N;i++) primary[i] = 1;
+    int i, count=0;
+    int temp;
 
     for (i=0;i<N;i++) {
-        scanf("%d", &number[i]);
+        scanf("%d", &temp);
+        number[i] = temp;
+        primary[i] = temp;
     }
 
-    //qsort(number, N, sizeof(int), comfunc);
+    // for (i=0;i<N;i++) printf("%d %d\n", number[i], primary[i]);return 0;
+
+    qsort(number, N, sizeof(int), comfunc);
 
     for (i=0;i<N;i++) {
-        j=0;
-        while (j<i && primary[i]) if (number[j++]>number[i]) primary[i]=0;
-
-        j=i+1;
-        while (j<N && primary[i]) if (number[j++]<number[i]) primary[i]=0;
-
-        if (primary[i]) count++;
+        if (number[i] == primary[i]) count++;
     }
 
     printf("%d\n", count);
     for (i=0;i<N;i++) {
-        if (primary[i]) {
+        if (primary[i]==number[i]) {
             printf("%d", number[i]);
             if (count--!=1) printf(" ");
         }
