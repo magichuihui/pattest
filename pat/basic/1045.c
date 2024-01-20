88a6f9651b848344a48c34f7c606a1a51ba129da
magichuihui <magichuihui@gmail.com>
2024-01-20 21:59:14 +0800

    fix: pat 1045
    create a max array of the left side of the given array, and a min array of the right side, instead of sorting the given numbers.

--- pat/basic/1045.c
+++ pat/basic/1045.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 100000
+#define MAX_SIZE 100010
+
+#define max(a, b) (a) > (b) ? (a) : (b)
+#define min(a, b) (a) < (b) ? (a) : (b)
 
 int comfunc(const void *a, const void *b) {
     return *(int *)a - *(int *)b;
@@ -9,33 +12,28 @@ int comfunc(const void *a, const void *b) {
 
 int main(void) {
     int N;
-    int number[MAX_SIZE], primary[MAX_SIZE];
+    int number[MAX_SIZE], max_left[MAX_SIZE], min_right[MAX_SIZE];
     scanf ("%d", &N);
     
     int i, count=0;
-    int temp;
 
     for (i=0;i<N;i++) {
-        scanf("%d", &temp);
-        number[i] = temp;
-        primary[i] = temp;
+        scanf("%d", &number[i]);
+        max_left[i] = 0;
+        min_right[i] = 1E9 + 1;
     }
-
-    // for (i=0;i<N;i++) printf("%d %d\n", number[i], primary[i]);return 0;
-
-    qsort(number, N, sizeof(int), comfunc);
-
-    for (i=0;i<N;i++) {
-        if (number[i] == primary[i]) count++;
-    }
-
+    for (i=1;i<N;i++) max_left[i] = max(max_left[i-1], number[i-1]);
+    for (i=N-2;i>=0;i--) min_right[i] = min(min_right[i+1], number[i+1]);
+ 
+    for (i=0;i<N;i++) if (number[i] > max_left[i] && number[i] < min_right[i]) count++;
     printf("%d\n", count);
+
     for (i=0;i<N;i++) {
-        if (primary[i]==number[i]) {
+        if (number[i] > max_left[i] && number[i] < min_right[i]) {
             printf("%d", number[i]);
             if (count--!=1) printf(" ");
         }
     }
     printf("\n");
-
+   
 }
