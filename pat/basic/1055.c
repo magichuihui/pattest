2afe72016c73fe9e0a105e75f5b610da904a12bf
kyra <magichuihui@gmail.com>
2024-01-24 11:16:16 +0800

    feat: pat 1055
    still need to improve, such as `person *res[10000];`, should use dynamic array.

--- pat/basic/1055.c
+++ pat/basic/1055.c
@@ -5,7 +5,7 @@
 typedef struct person {
     char name[9];
     int height;
-    person *next;
+    struct person *next;
 } person;
 
 void insert(person *p, char *name, int height);
@@ -52,10 +52,44 @@ int main(void) {
     }
 
     p = head->next;
-    while (p!=NULL) {
-        printf("%s %d\n", p->name, p->height);
+    // while (p!=NULL) {
+    //     printf("%s %d\n", p->name, p->height);
+    //     p = p->next;
+    // }
+
+
+    int q = N / K, remainder = N % K, m = q+remainder, mid;
+    int j, k, base=0;
+    person *res[10000];
+
+    for (i=0;i<K;i++) {
+        mid = m/2+1;
+        res[base + mid-1] = p;
         p = p->next;
+        for (j=1;j<m;j++) {
+            // 中间的左边还是右边
+            k = j % 2 == 0 ? 1 : -1;
+            res[base+mid+k*(j+1)/2-1] = p;
+            p=p->next;
+        }
+
+        base += m;
+        m = q;
     }
+
+    m = q+remainder;
+    j = 0;
+    for (i=0;i<K;i++) {
+        while(m>0) {
+            printf("%s", res[j]->name);
+            if (m!=1) printf(" ");
+            j++;
+            m--;
+        }
+        printf("\n");
+        m=q;
+    }
+
 }
 
 void insert(person *p, char *name, int height) {
